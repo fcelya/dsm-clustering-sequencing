b5b82cec4ff305b4d999fc67cff59b1bfc59433f
fcelya <92804587+fcelya@users.noreply.github.com>
2022-02-22 18:54:07 -0500

    New improved version

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-import pandas as pd
 from string import ascii_uppercase as alphabet
 
 size = 7
-elements = []
+num2let = []
+let2num = {}
 for i in range(size):
-    elements.append(tuple([alphabet[i], i]))
+    num2let.append(alphabet[i])
+    let2num[alphabet[i]] = i
 
-dsm = {
-    elements[0]: [5],
-    elements[1]: [0, 2, 3, 6],
-    elements[2]: [3, 6],
-    elements[3]: [1, 2, 4, 6],
-    elements[4]: [0, 3, 5],
-    elements[5]: [0, 4],
-    elements[6]: [1, 2, 3],
+dsm_original = {
+    num2let[0]: ["F"],
+    num2let[1]: ["A", "C", "D", "G"],
+    num2let[2]: ["D", "G"],
+    num2let[3]: ["B", "C", "E", "G"],
+    num2let[4]: ["A", "D", "F"],
+    num2let[5]: ["A", "E"],
+    num2let[6]: ["B", "C", "D"],
 }
 
 
-def measure(dsm, weight=1):
+def measure(dsm_num, weight=1):
     points = 0
-    for key in dsm:
-        for x in dsm[key]:
-            point = x - key[1]
+    for key in dsm_num:
+        for x in dsm_num[key]:
+            point = x - key
             if point < 0:
                 points -= weight * point
             else:
                 points += point
     return points
 
-    # https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python/
 
-
-def permutation(lst):
+# https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python/
+# Algorithm to generate permutations obtained from the above page
+def permutation_generator(lst):
     if len(lst) == 0:
         return []
     if len(lst) == 1:
@@ -40,32 +41,37 @@ def permutation(lst):
     for i in range(len(lst)):
         m = lst[i]
         remLst = lst[:i] + lst[i + 1 :]
-        for p in permutation(remLst):
+        for p in permutation_generator(remLst):
             l.append([m] + p)
     return l
 
 
+def letdsm2numdsm(dsm_orginal, permutation, let2num):
+    dsm_num = {}
+    for key in dsm_original:
+        l = []
+        for item in dsm_original[key]:
+            l.append(permutation[let2num[item]])
+        dsm_num[permutation[let2num[key]]] = l
+    return dsm_num
+
+
 positions = list(range(size))
-permutations = permutation(positions)
+permutations = permutation_generator(positions)
 iterations = len(permutations)
 
 i = 0
+winner = {"letters": [], "points": 999, "dsm": {}}
 winner_points = 999
 for permutation in permutations:
     i += 1
-    dsm_temp = {}
-    for key in dsm:
-        connections = dsm[key]
-        new_connections = []
-        for c in connections:
-            new_connections.append(permutation[c])
-        dsm_temp[tuple([key[0], permutation[key[1]]])] = new_connections
+    dsm_temp = letdsm2numdsm(dsm_original, permutation, let2num)
     points = measure(dsm_temp)
-    if points < winner_points:
-        winner_points = points
-        letters = []
-        for i in permutation:
-            letters.append(alphabet[i])
-        winner = (letters, winner_points)
-    print(f"Working... {i}/{iterations}")
+    if points < winner["points"]:
+        winner["points"] = points
+        letters = list(range(size))
+        for j in range(size):
+            letters[permutation[j]] = num2let[j]
+        winner["letters"] = letters
+        winner["dsm"] = dsm_temp
 print(winner)
